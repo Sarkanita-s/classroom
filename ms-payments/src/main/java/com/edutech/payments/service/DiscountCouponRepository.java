35bcb6ef8e28eae464eb9a7b36163d2ae30111af
CNS-Kairos <astt.greg@gmail.com>
2025-06-29 16:56:45 -0400

    Se añadieron las interfaces en service para ms-payments

---
+++ ms-payments/src/main/java/com/edutech/payments/service/DiscountCouponRepository.java
@@ -0,0 +1,11 @@
+package com.edutech.payments.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edutech.payments.entity.DiscountCoupon;
+
+@Repository
+public interface DiscountCouponRepository extends JpaRepository<DiscountCoupon, Integer> {
+
+}
---
+++ ms-payments/src/main/java/com/edutech/payments/service/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.edutech.payments.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edutech.payments.entity.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
+
+}
