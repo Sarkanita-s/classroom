7d429642cb574deb4a6bed2fc2f2eb3dfe2b3c51
Chofa's <sofigmez9@gmail.com>
2025-06-30 01:53:13 -0400

    Actualizacion de User entity se agregan getters y setters

--- ms-users/src/main/java/com/edutech/users/entity/User.java
+++ ms-users/src/main/java/com/edutech/users/entity/User.java
@@ -44,4 +44,20 @@ public class User {
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
