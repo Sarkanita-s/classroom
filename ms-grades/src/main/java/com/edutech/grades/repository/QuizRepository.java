932ad86e0b29526f4c35252c378b83d416acdf45
Chofa's <sofigmez9@gmail.com>
2025-06-29 23:19:24 -0400

    Creacion de Package Repository y sus archivos

---
+++ ms-grades/src/main/java/com/edutech/grades/repository/QuizRepository.java
@@ -0,0 +1,9 @@
+package com.edutech.grades.repository;
+
+import com.edutech.grades.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+}
